8ae3cd87dcf8b20f132d10227a3b093e53b9de08
HapiFive <>
2023-10-26 23:31:58 +0800

    初版完工

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'dart:convert';
+import 'dart:developer';
 import 'dart:io';
 import 'dart:math';
+import 'dart:typed_data';
+import 'dart:ui';
+import 'package:flutter/painting.dart';
 
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:path/path.dart' as path_util;
+import 'package:super_drag_and_drop/super_drag_and_drop.dart';
 import 'components/resizable_rectangle.dart';
 
 void main() {
@@ -70,6 +75,11 @@ class _MyHomePageState extends State<MyHomePage> {
   int selectedLabel = -1;
   String newClassName = "";
   late ImageStreamListener listener;
+  bool moveToTrain = false;
+  String? trainImageDirectory;
+  String? trainLabelDirectory;
+  bool changeLabelList = false;
+  bool changeClassList = false;
   @override
   void initState() {
     super.initState();
@@ -83,15 +93,21 @@ class _MyHomePageState extends State<MyHomePage> {
       programSetting = value;
       imageDirectory = value.getString('imageDirectory');
       labelDirectory = value.getString('labelDirectory');
+      trainImageDirectory = value.getString('trainImageDirectory');
+      trainLabelDirectory = value.getString('trainLabelDirectory');
+      moveToTrain = value.getBool("moveToTrain") ?? false;
       if (imageDirectory != null) {
         imageFile = findFilesInDirectory(Directory(imageDirectory!));
       }
       if (labelDirectory != null) {
-        File('$labelDirectory/classes.txt').readAsString().then((value) {
-          setState(() {
-            labelName = value.split(RegExp(r'\r\n|\n\r|\r|\n'));
+        File classesFile = File('$labelDirectory/classes.txt');
+        if (classesFile.existsSync()) {
+          classesFile.readAsString().then((value) {
+            setState(() {
+              labelName = value.split(RegExp(r'\r\n|\n\r|\r|\n'));
+            });
           });
-        });
+        }
       }
     });
   }
@@ -113,10 +129,16 @@ class _MyHomePageState extends State<MyHomePage> {
                     .getDirectoryPath(initialDirectory: imageDirectory)
                     .then((selectedDirectory) {
                   if (selectedDirectory != null) {
-                    imageDirectory = selectedDirectory;
                     setState(() {
+                      showIndexImage(-1);
+                      imageDirectory = selectedDirectory;
                       imageFile =
                           findFilesInDirectory(Directory(selectedDirectory));
+                      if (imageFile.isNotEmpty) {
+                        nowShowImageIndex = 0;
+                      } else {
+                        nowShowImageIndex = -1;
+                      }
                     });
                     programSetting.setString(
                         'imageDirectory', selectedDirectory);
@@ -135,6 +157,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     .then((selectedDirectory) {
                   if (selectedDirectory != null) {
                     setState(() {
+                      showIndexImage(-1);
                       labelDirectory = selectedDirectory;
                     });
                     programSetting.setString(
@@ -150,6 +173,76 @@ class _MyHomePageState extends State<MyHomePage> {
                 });
               },
               child: const Text('选择标签目录'),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            SizedBox(
+              width: 150,
+              child: CheckboxListTile(
+                enabled:
+                    trainImageDirectory != null && trainLabelDirectory != null,
+                controlAffinity: ListTileControlAffinity.leading,
+                contentPadding: const EdgeInsets.all(0),
+                title: const Text(
+                  "标注修改时移入训练目录",
+                  style: TextStyle(fontSize: 14),
+                ),
+                value: moveToTrain,
+                onChanged: (value) => setState(() {
+                  moveToTrain = !moveToTrain;
+                  programSetting.setBool("moveToTrain", moveToTrain);
+                }),
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                FilePicker.platform
+                    .getDirectoryPath(initialDirectory: trainImageDirectory)
+                    .then((selectedDirectory) {
+                  if (selectedDirectory != null) {
+                    setState(() {
+                      trainImageDirectory = selectedDirectory;
+                    });
+                    programSetting.setString(
+                        'trainImageDirectory', selectedDirectory);
+                  }
+                });
+              },
+              child: const Text('选择训练图片目录'),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                FilePicker.platform
+                    .getDirectoryPath(initialDirectory: trainLabelDirectory)
+                    .then((selectedDirectory) {
+                  if (selectedDirectory != null) {
+                    setState(() {
+                      trainLabelDirectory = selectedDirectory;
+                    });
+                    programSetting.setString(
+                        'trainLabelDirectory', selectedDirectory);
+                  }
+                });
+              },
+              child: const Text('选择训练标签目录'),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                changeLabelList = false;
+                changeClassList = false;
+                showIndexImage(nowShowImageIndex);
+              },
+              child: const Text('还原标注'),
             ),
             // const SizedBox(
             //   height: 20,
@@ -211,17 +304,20 @@ class _MyHomePageState extends State<MyHomePage> {
                 },
                 child: GestureDetector(
                   onDoubleTapDown: (details) => setState(() {
-                    labelPositionList.add(<double>[
-                      0,
-                      (details.localPosition.dx -
-                              (constraints.maxWidth - actuallyWidth) / 2) /
-                          max(actuallyWidth, 1),
-                      (details.localPosition.dy -
-                              (constraints.maxHeight - actuallyHeight) / 2) /
-                          max(actuallyHeight, 1),
-                      0,
-                      0
-                    ]);
+                    if (nowShowImageIndex >= 0 &&
+                        nowShowImageIndex < imageFile.length) {
+                      labelPositionList.add(<double>[
+                        0,
+                        (details.localPosition.dx -
+                                (constraints.maxWidth - actuallyWidth) / 2) /
+                            max(actuallyWidth, 1),
+                        (details.localPosition.dy -
+                                (constraints.maxHeight - actuallyHeight) / 2) /
+                            max(actuallyHeight, 1),
+                        0,
+                        0
+                      ]);
+                    }
                   }),
                   onHorizontalDragEnd: (details) {
                     double ratio = details.velocity.pixelsPerSecond.dx /
@@ -242,6 +338,24 @@ class _MyHomePageState extends State<MyHomePage> {
                     }
                   },
                   child: showImage,
+                  // child: DragItemWidget(
+                  //   dragItemProvider: (request) async {
+                  //     // DragItem represents the content begin dragged.
+                  //     final item = DragItem();
+                  //     // Add data for this item that other applications can read
+                  //     // on drop. (optional)
+                  //     item.add(Formats.png(await createImageData()));
+                  //     item.add(Formats.png(Uint8List.fromList(utf8.encode(
+                  //         r"D:\Code\Python\IdentifyingEmoticons\success\image\$$$QGXZTA]YQZLS5@I)$]IY.jpg"))));
+                  //     return item;
+                  //   },
+                  //   allowedOperations: () => [DropOperation.copy],
+                  //   // DraggableWidget represents the actual draggable area. It looks
+                  //   // for parent DragItemWidget in widget hierarchy to provide the DragItem.
+                  //   child: DraggableWidget(
+                  //     child: showImage,
+                  //   ),
+                  // ),
                 ),
               )
             ];
@@ -270,6 +384,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 top: top,
                 text: showName,
                 onResizeOrMove: (detail) => setState(() {
+                  changeLabelList = true;
                   labelPosition[1] = labelPosition[1] +
                       (detail.left + detail.right) / 2 / actuallyWidth;
                   labelPosition[2] = labelPosition[2] +
@@ -284,6 +399,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       0);
                 }),
                 onLongPress: () => setState(() {
+                  changeLabelList = true;
                   labelPositionList.removeAt(index);
                 }),
                 onTextTap: () => setState(() {
@@ -318,6 +434,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       if (index < labelName.length) {
                         return InkWell(
                           onTap: () => setState(() {
+                            changeLabelList = true;
                             int temp = selectedLabel;
                             selectedLabel = -1;
                             labelPositionList[temp][0] = index.toDouble();
@@ -362,6 +479,8 @@ class _MyHomePageState extends State<MyHomePage> {
                                 alignment: Alignment.centerRight,
                                 child: GestureDetector(
                                   onTap: () => setState(() {
+                                    changeLabelList = true;
+                                    changeClassList = true;
                                     int temp = selectedLabel;
                                     selectedLabel = -1;
                                     labelPositionList[temp][0] =
@@ -443,18 +562,92 @@ class _MyHomePageState extends State<MyHomePage> {
 
   List<File> findFilesInDirectory(Directory directory) {
     List<File> files = [];
-    // 遍历目录中的内容（包括子目录）
-    directory.listSync(recursive: true).where((fileSystemEntity) {
-      return fileSystemEntity is File &&
-          fileSystemEntity.path.toLowerCase().endsWith('.jpg');
-    }).forEach((fileSystemEntity) {
-      files.add(fileSystemEntity as File);
-    });
+    if (directory.existsSync()) {
+      // 遍历目录中的内容（包括子目录）
+      directory.listSync(recursive: true).where((fileSystemEntity) {
+        return fileSystemEntity is File &&
+            fileSystemEntity.path.toLowerCase().endsWith('.jpg');
+      }).forEach((fileSystemEntity) {
+        files.add(fileSystemEntity as File);
+      });
+    }
     return files;
   }
 
   void showIndexImage(int index) {
-    if (imageFile.isNotEmpty) {
+    if (changeLabelList) {
+      int operationIndex = nowShowImageIndex;
+      File operationFile = imageFile[operationIndex];
+      IOSink ioSink = File(path_util.setExtension(
+              operationFile.path
+                  .replaceRange(0, imageDirectory!.length, labelDirectory!),
+              '.txt'))
+          .openWrite();
+      for (var element in labelPositionList) {
+        double centerX = element[1];
+        double centerY = element[2];
+        double width = element[3];
+        double height = element[4];
+        if (centerX - width / 2 < 0) {
+          double newRight = centerX + width / 2;
+          centerX = newRight / 2;
+          width = newRight;
+        }
+        if (centerX + width / 2 > 1) {
+          double newLeft = centerX - width / 2;
+          centerX = (1 + newLeft) / 2;
+          width = 1 - newLeft;
+        }
+        if (centerY - height / 2 < 0) {
+          double newBottom = centerY + height / 2;
+          centerY = newBottom / 2;
+          height = newBottom;
+        }
+        if (centerY + height / 2 > 1) {
+          double newTop = centerY - height / 2;
+          centerY = (1 + newTop) / 2;
+          height = 1 - newTop;
+        }
+        ioSink.writeln(
+            "${element[0].toInt()} ${centerX.toStringAsFixed(6)} ${centerY.toStringAsFixed(6)} ${width.toStringAsFixed(6)} ${height.toStringAsFixed(6)}");
+      }
+      ioSink.flush().then((value) => ioSink.close().then((value) {
+            String? tempImageDirectory = trainImageDirectory;
+            String? tempLabelDirectory = trainLabelDirectory;
+            String? temp = labelDirectory;
+            if (moveToTrain &&
+                tempImageDirectory != null &&
+                tempLabelDirectory != null &&
+                temp != null) {
+              setState(() {
+                imageFile.removeAt(operationIndex);
+                String baseName = path_util.basename(operationFile.path);
+                operationFile
+                    .rename(path_util.join(tempImageDirectory, baseName));
+                File labelFile = File(path_util.join(temp, baseName));
+                labelFile.exists().then((value) {
+                  String toPath = path_util.join(tempLabelDirectory,
+                      path_util.setExtension(baseName, ".txt"));
+                  if (value) {
+                    labelFile.rename(toPath);
+                  } else {
+                    File(toPath).create(recursive: true);
+                  }
+                });
+              });
+            }
+          }));
+    }
+    if (changeClassList) {
+      IOSink ioSink = File('$labelDirectory/classes.txt').openWrite();
+      for (var element in labelName) {
+        ioSink.writeln(element.trim());
+      }
+      ioSink.flush().then((value) => ioSink.close());
+    }
+    changeLabelList = false;
+    changeClassList = false;
+    if (imageFile.isNotEmpty && index != -1) {
       index = index.clamp(0, imageFile.length - 1);
       imageWidth = 0;
       imageHeight = 0;
@@ -474,27 +667,47 @@ class _MyHomePageState extends State<MyHomePage> {
             .resolve(const ImageConfiguration())
             .addListener(listener);
       });
-      showLabelFile.readAsString(encoding: utf8).then((value) {
-        List<List<double>> tempLabelPositionList = [];
-        List<double> tempDoubleList = [];
-        List<String> labelStringList = value.split(RegExp(r'\r\n|\n\r|\r|\n'));
-        for (var element in labelStringList) {
-          List<String> elementSplit = element.split(RegExp(r"\s+"));
-          if (elementSplit.length >= 5) {
-            for (var str in elementSplit) {
-              double n = double.parse(str);
-              tempDoubleList.add(n);
+      showLabelFile.exists().then((value) {
+        if (value) {
+          showLabelFile.readAsString(encoding: utf8).then((value) {
+            List<List<double>> tempLabelPositionList = [];
+            List<double> tempDoubleList = [];
+            List<String> labelStringList =
+                value.split(RegExp(r'\r\n|\n\r|\r|\n'));
+            for (var element in labelStringList) {
+              List<String> elementSplit = element.split(RegExp(r"\s+"));
+              if (elementSplit.length >= 5) {
+                for (var str in elementSplit) {
+                  double n = double.parse(str);
+                  tempDoubleList.add(n);
+                }
+              }
+              if (tempDoubleList.length == 5) {
+                tempLabelPositionList.add(tempDoubleList);
+              }
+              tempDoubleList = [];
             }
-          }
-          if (tempDoubleList.length == 5) {
-            tempLabelPositionList.add(tempDoubleList);
-          }
-          tempDoubleList = [];
+            setState(() {
+              labelPositionList = tempLabelPositionList;
+            });
+          });
+        } else {
+          setState(() {
+            labelPositionList = [];
+          });
         }
-        setState(() {
-          labelPositionList = tempLabelPositionList;
-        });
       });
     }
+  }
+
+  Future<Uint8List> createImageData() async {
+    // 加载图片
+    final bytes = await imageFile[nowShowImageIndex].readAsBytes();
+    final codec = await instantiateImageCodec(bytes);
+    final frameInfo = await codec.getNextFrame();
+    final image = frameInfo.image;
+    final data = await image.toByteData(format: ImageByteFormat.png);
+    Uint8List l = data!.buffer.asUint8List();
+    return l;
   }
 }
